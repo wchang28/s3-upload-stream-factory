73279b18f87ea4cc7f47836e490f0ae8ff9251c2
wchang28 <wchang28@hotmail.com>
2016-08-30 16:37:44 -0400

    trgrtgwrt

--- ts/index.ts
+++ ts/index.ts
@@ -4,8 +4,6 @@ import {S3} from 'aws-sdk';
 import * as _ from 'lodash';
 import * as stream from 'stream';
 
-//let s3Stream = require('s3-upload-stream')(new S3());
-
 export interface KeyMaker {
     (params: busboyPipe.FilePipeParams) : string
 }
@@ -24,23 +22,6 @@ class Transformer extends stream.Transform {
         callback(null, chunk);
     }
 }
-
-/*
-export function get(options: Options) : busboyPipe.WriteStreamFactory {
-    return ((params: busboyPipe.FilePipeParams) : busboyPipe.WriteStreamInfo => {
-        let s3Params: any = {
-            "Bucket": options.Bucket,
-            "Key": options.KeyMaker(params)
-        };
-        if (options.additonalS3Options) s3Params = _.assignIn(s3Params, options.additonalS3Options);
-        let upload: stream.Writable = s3Stream.upload(s3Params);
-        upload.on('uploaded', (details:any) => {
-            upload.emit('close');
-        });
-        return {stream: upload, streamInfo: s3Params};
-    });
-}
-*/
 
 export function get(options: Options) : busboyPipe.WriteStreamFactory {
     return ((params: busboyPipe.FilePipeParams) : busboyPipe.WriteStreamInfo => {
