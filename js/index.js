f8d7b6e88e43d735fc38926f8fd618bf3312124e
wchang28 <wchang28@hotmail.com>
2017-05-09 10:45:10 -0400

    upgrade to es2017

--- js/index.js
+++ js/index.js
@@ -1,16 +1,22 @@
 "use strict";
-var __extends = (this && this.__extends) || function (d, b) {
-    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
-    function __() { this.constructor = d; }
-    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-};
-var AWS = require('aws-sdk');
-var _ = require('lodash');
-var stream = require('stream');
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+var AWS = require("aws-sdk");
+var _ = require("lodash");
+var stream = require("stream");
 var Transformer = (function (_super) {
     __extends(Transformer, _super);
     function Transformer(opts) {
-        _super.call(this, opts);
+        return _super.call(this, opts) || this;
     }
     Transformer.prototype._transform = function (chunk, encoding, callback) {
         callback(null, chunk);
